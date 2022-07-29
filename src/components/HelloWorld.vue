1fcbc4f49e31b205dbeb2f998cf4cdaa268ade5a
Yuyan sun <yuyan.sun@thoughtworks.com>
2022-07-30 00:19:08 +0800

    [yuyan] init project

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,19 +2,20 @@
 defineProps({
   msg: {
     type: String,
-    required: true
   }
 })
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+    <h1> 文明典范·你我共创 </h1>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <button>点击签约</button>
   </div>
 </template>
 
@@ -22,7 +23,6 @@ defineProps({
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
-  top: -10px;
 }
 
 h3 {
