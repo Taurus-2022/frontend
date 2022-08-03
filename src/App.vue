851db4a7f5567258f49f3e1ac82b6cda83661f93
Yuyan sun <yuyan.sun@thoughtworks.com>
2022-08-04 00:15:33 +0800

    [yuyan] just for test

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,51 @@
 <script setup>
-import { onMounted } from 'vue';
-import {Swiper,SwiperSlide} from 'swiper/vue';
-import {Pagination, EffectFade} from 'swiper';
+// import { onMounted } from 'vue';
 import "swiper/css";
 import 'swiper/css/effect-fade';
-
-import "./style.css";
-import Page1 from './components/page-1.vue';
-import Page2 from './components/page-2.vue';
+// import Page1 from './components/page-1.vue';
+// import Page2 from './components/page-2.vue';
 import Page3 from './components/page-3.vue';
-import Page4 from './components/page-4.vue';
+// import Page4 from './components/page-4.vue';
 
-onMounted(() => {
-  console.log(`the component is now mounted.`);
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  const vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-  // We listen to the resize event
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-    setRemUnit();
-  });
-
-  // 抵消字体放大的影响
-  const DEFAULT_FONT_SIZE = 16;
-  const docEl = document.documentElement;
-  let userWebsetFontDefaultSize = DEFAULT_FONT_SIZE;
-  // 用原生方法获取用户设置的浏览器的字体大小(兼容ie)
-  if (docEl.currentStyle) {
-    userWebsetFontDefaultSize = docEl.currentStyle['fontSize'];
-  } else {
-    userWebsetFontDefaultSize = getComputedStyle(docEl, false)['fontSize'];
-  }
-  // 取数字部分
-  userWebsetFontDefaultSize = parseFloat(userWebsetFontDefaultSize);
-  // 将实际的字号除以默认字号
-  const sizeScale = userWebsetFontDefaultSize / DEFAULT_FONT_SIZE;
-  function setRemUnit() {
-    const rem = 16 / sizeScale;
-    docEl.style.fontSize = rem + 'px';
-  }
-
-  setRemUnit();
-});
+// onMounted(() => {
+//   console.log(`the component is now mounted.`);
+//   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+//   const vh = window.innerHeight * 0.01;
+//   // Then we set the value in the --vh custom property to the root of the document
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+//   // We listen to the resize event
+//   window.addEventListener('resize', () => {
+//     // We execute the same script as before
+//     const vh = window.innerHeight * 0.01;
+//     document.documentElement.style.setProperty('--vh', `${vh}px`);
+//     setRemUnit();
+//   });
+//
+//   // 抵消字体放大的影响
+//   const DEFAULT_FONT_SIZE = 16;
+//   const docEl = document.documentElement;
+//   let userWebsetFontDefaultSize = DEFAULT_FONT_SIZE;
+//   // 用原生方法获取用户设置的浏览器的字体大小(兼容ie)
+//   if (docEl.currentStyle) {
+//     userWebsetFontDefaultSize = docEl.currentStyle['fontSize'];
+//   } else {
+//     userWebsetFontDefaultSize = getComputedStyle(docEl, false)['fontSize'];
+//   }
+//   // 取数字部分
+//   userWebsetFontDefaultSize = parseFloat(userWebsetFontDefaultSize);
+//   // 将实际的字号除以默认字号
+//   const sizeScale = userWebsetFontDefaultSize / DEFAULT_FONT_SIZE;
+//   function setRemUnit() {
+//     const rem = 16 / sizeScale;
+//     docEl.style.fontSize = rem + 'px';
+//   }
+//
+//   setRemUnit();
+// });
 </script>
 
 <template>
-  <swiper
-:direction="'vertical'"
-          :effect="'fade'"
-          :modules="[Pagination, EffectFade]" :pagination="{clickable:false, showsPagination: false}"
-  >
-    <swiper-slide>
-      <page1/>
-    </swiper-slide>
-    <swiper-slide>
-      <page2/>
-    </swiper-slide>
-    <swiper-slide>
       <page3/>
-    </swiper-slide>
-    <swiper-slide>
-      <page4/>
-    </swiper-slide>
-  </swiper>
 </template>
 
 <style scoped></style>
--- src/components/page-3.vue
+++ src/components/page-3.vue
@@ -1,4 +1,5 @@
 <script setup>
+// import imagePath from '@/assets/p3.png'
 defineProps({
   msg: {
     type: String,
@@ -8,9 +9,25 @@ defineProps({
 </script>
 
 <template>
-  <img src='/src/assets/p3.png'>
+  <div class='container'>
+    <p>dddddd</p>
+    <img src='/src/assets/p3.png'>
+    <p>dddddd</p>
+  </div>
+
 </template>
 
 <style scoped>
+.container {
+  overflow: auto; /* adds scrollbars */
+  background-color: blue;
+  position: relative;
+  -webkit-overflow-scrolling: touch
+}
+div img{
+  width: 100%;
+  min-height: 1003px;
+  vertical-align: bottom;
+}
 
 </style>
