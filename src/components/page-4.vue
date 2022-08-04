2bf5317c8411a49c60fe93bd5a6c90265072ae70
Pengfei Qian <302539864@qq.com>
2022-08-05 01:16:19 +0800

    feat: add turntable

--- src/components/page-4.vue
+++ src/components/page-4.vue
@@ -13,6 +13,12 @@ const store = useStore()
 <template>
   <div class='container'>
     <img class='background' src='/src/assets/p4.png' @click="store.firstPageIndex">
+    <div class='turntable-container'>
+      <img class='img1' src='src/assets/turntable.jpg'/>
+    </div>
+    <div class='pointer-container'>
+      <img class='img2' src='src/assets/pointer.jpg'/>
+    </div>
   </div>
 </template>
 
@@ -28,5 +34,30 @@ const store = useStore()
   width: 100vw;
   position: absolute;
 }
+  .turntable-container{
+    position: relative;
+    min-width: 360px;
+    top: 11rem;
+  }
+  .pointer-container{
+    position: relative;
+    min-width: 360px;
+    top:-2rem;
+  }
+  .img1{
+    max-width: 100%;
+    max-height: 100%;
+    width: 18rem;
+    display: block;
+    margin: 0 auto;
+  }
+  .img2{
+    max-width: 100%;
+    max-height: 100%;
+    width: 5.2rem;
+    display: block;
+    margin: 0 auto;
+  }
+
 }
 </style>
